88467a09d268cae611c90ba08d96faeb6b8594f7
Kataoka.R <dat175b044@gmail.com>
2017-07-07 16:00:33 +0900

    不要なカメラとライトを無効

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+	public static string NextScene = "";
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.O)) {
+			NextScene = "Game Over";
+		}
+		else if (Input.GetKeyDown (KeyCode.C)) {
+			NextScene = "Clear";
+		}
+
+		if (NextScene.Length > 0) {
+			SceneManager.LoadSceneAsync (NextScene,LoadSceneMode.Additive);
+			NextScene = "";
+			enabled = false;
+		}
+	}
+}
