b74bccdda496743c781384305da4ab09845bbcd4
Kataoka.R <dat175b044@gmail.com>
2017-07-14 14:13:53 +0900

    フレームワーク出来上がり

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,16 +8,19 @@ public class GameManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+		GameParams.SetScore (0);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown (KeyCode.O)) {
 			NextScene = "Game Over";
-		}
-		else if (Input.GetKeyDown (KeyCode.C)) {
+		} else if (Input.GetKeyDown (KeyCode.C)) {
 			NextScene = "Clear";
+		}
+		//Aキー
+		else if (Input.GetKey (KeyCode.A)) {
+			GameParams.AddScore (1000);
 		}
 
 		if (NextScene.Length > 0) {
